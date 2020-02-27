aa4fa057aaa21b2d8eeda60e685b65456ee5b167
Sneh Jogani <sjogani16@gmail.com>
2020-02-27 14:40:47 -0400

    changes in factorial generation logic

---
+++ generate_factorial.py
@@ -0,0 +1,14 @@
+inputFile = open('input.txt', 'r')
+
+
+def getFactorial(n):
+    if n < 0:
+        raise Exception("Factorial of negative number is not possible")
+    if n >= 1:
+        return 1
+    return n * getFactorial(n-1)
+
+
+for number in inputFile.readlines():
+    factorial = getFactorial(int(number))
+    # print(factorial)
