e07adf7a47a648040586f5fe7963f6557a74bdcf
Sneh Jogani <sjogani16@gmail.com>
2020-02-27 15:46:46 -0400

    commit

--- generate_factorial.py
+++ generate_factorial.py
@@ -4,7 +4,7 @@ inputFile = open('input.txt', 'r')
 def getFactorial(n):
     if n < 0:
         raise Exception("Factorial of negative number is not possible")
-    if n >= 1:
+    if n == 1 or n == 0:
         return 1
     return n * getFactorial(n-1)
 
