d6ec22c6358a6a9364e23ab2f52edd61046af252
Sneh Jogani <sjogani16@gmail.com>
2020-02-27 14:56:31 -0400

    implemented fibonacci series generation logic

---
+++ get_fibonnaci.py
@@ -0,0 +1,15 @@
+inputFile = open('input.txt', 'r')
+
+numberList = inputFile.readlines()
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(1, n):
+        a, b = b, a + b
+    return b
+
+
+for number in numberList:
+    print('Fibonacci Series upto', number)
+    print([fibonacci(i) for i in range(0, int(number))])
+    print('\n')
